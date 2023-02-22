4903c6a2440d6d31770fa1e7cacf701e96b3ca91
matthewdowns <matthewdowns95@gmail.com>
2023-02-22 10:40:28 +0000

    Add health, prayer, and run energy to canvas overlay with toggles on colours and visibility

--- src/main/java/com/example/ExampleConfig.java
+++ src/main/java/com/example/ExampleConfig.java
@@ -4,16 +4,64 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
+import java.awt.*;
+
 @ConfigGroup("example")
 public interface ExampleConfig extends Config
 {
+
+	@ConfigItem
+			(
+					position = 1,
+					keyName = "showHealth",
+					name = "Show Health",
+					description = "Render Health overlay"
+			)
+
+	default boolean renderHealth() { return false; }
+
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+			position = 2,
+			keyName = "healthColour",
+			name = "Health Colour",
+			description = "Choose the background colour of the health bar"
 	)
-	default String greeting()
-	{
-		return "Hello";
-	}
+	default Color healthColour() { return Color.RED; }
+	@ConfigItem
+			(
+					position = 3,
+					keyName = "showPrayer",
+					name = "Show Prayer",
+					description = "Render Prayer overlay"
+			)
+
+	default boolean renderPrayer() { return false; }
+
+	@ConfigItem(
+			position = 4,
+			keyName = "prayerColour",
+			name = "Prayer Colour",
+			description = "Choose the background colour of the prayer bar"
+	)
+	default Color prayerColour() { return Color.CYAN; }
+
+	@ConfigItem
+			(
+					position = 5,
+					keyName = "showEnergy",
+					name = "Show Energy",
+					description = "Render Run Energy overlay"
+			)
+
+	default boolean renderEnergy() { return false; }
+
+	@ConfigItem(
+			position = 6,
+			keyName = "energyColour",
+			name = "Energy Colour",
+			description = "Choose the background colour of the run energy bar"
+	)
+	default Color energyColour() { return Color.orange; }
+
+
 }
--- src/main/java/com/example/MiniBarsPlugin.java
+++ src/main/java/com/example/MiniBarsPlugin.java
@@ -3,20 +3,21 @@ package com.example;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.ScriptPreFired;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
 
 @Slf4j
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class MiniBarsPlugin extends Plugin
 {
 	@Inject
 	private Client client;
@@ -24,25 +25,30 @@ public class ExamplePlugin extends Plugin
 	@Inject
 	private ExampleConfig config;
 
+
+	@Inject
+	private MiniBarsOverlay overlay;
+
+	@Inject
+	private OverlayManager overlayManager;
+
 	@Override
-	protected void startUp() throws Exception
+	protected void startUp()
 	{
-		log.info("Example started!");
+		overlayManager.add(overlay);
 	}
 
 	@Override
-	protected void shutDown() throws Exception
+	protected void shutDown()
 	{
-		log.info("Example stopped!");
+		overlayManager.remove(overlay);
+		client.getWidget(WidgetInfo.EXPERIENCE_TRACKER).setHidden(false);
 	}
 
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	public void onScriptPreFired (ScriptPreFired scriptPreFired)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
+
 	}
 
 	@Provides
