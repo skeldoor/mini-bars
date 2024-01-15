83fe3efccc0fe8aa07a0b3d3fec2e6587f7dd4f1
Seung Hahm <73006826+qt31415926535-femboy@users.noreply.github.com>
2024-01-14 20:23:49 -0800

    update to my code

--- src/main/java/com/skeldoor/MiniBarsConfig.java
+++ src/main/java/com/skeldoor/MiniBarsConfig.java
@@ -1,138 +1,244 @@
-package com.skeldoor;
+package net.runelite.client.plugins.minibars;
 
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.*;
 import net.runelite.client.ui.overlay.components.ComponentConstants;
 import java.awt.*;
 
 @ConfigGroup("Mini Bars")
 public interface MiniBarsConfig extends Config
 {
-
-	@ConfigItem
-			(
-					position = 1,
-					keyName = "showHealth",
-					name = "Show Health",
-					description = "Render Health overlay"
-			)
-
-	default boolean renderHealth() { return false; }
+	String GROUP = "minibars";
 
 	@ConfigItem(
-			position = 2,
-			keyName = "healthColour",
-			name = "Health Colour",
-			description = "Choose the background colour of the health bar"
+			position = 1,
+			keyName = "showHealth",
+			name = "Show Health",
+			description = "Render Health overlay"
 	)
-	default Color healthColour() { return Color.RED; }
+	default boolean renderHealth() { return false; }
+
+	@ConfigSection(
+			position = 2,
+			name = "Health Bar Settings",
+			description = "Health Bar Settings"
+	)
+	String healthSettingsSection = "healthSettings";
 
 	@ConfigItem(
 			position = 3,
 			keyName = "healthSize",
-			name = "Health Size",
-			description = "Choose the size of the Health bar"
+			name = "Health Bar Size",
+			description = "Choose the size of the Health bar",
+			section = healthSettingsSection
 	)
-	default Dimension healthSize() { return new Dimension(ComponentConstants.STANDARD_WIDTH, 0); }
-	@ConfigItem
-			(
-					position = 4,
-					keyName = "showPrayer",
-					name = "Show Prayer",
-					description = "Render Prayer overlay"
-			)
+	@Range(
+			min = MiniBarsConstants.WIDTH_MIN
+	)
+	default Dimension healthSize() { return new Dimension(ComponentConstants.STANDARD_WIDTH, MiniBarsConstants.STANDARD_HEIGHT); }
 
-	default boolean renderPrayer() { return false; }
+	@ConfigItem(
+			position = 4,
+			keyName = "healthFullnessDirection",
+			name = "Health Fullness Direction",
+			description = "Choose the direction of fullness of the Health bar",
+			section = healthSettingsSection
+	)
+	default FullnessDirection healthFullnessDirection()
+	{
+		return FullnessDirection.RIGHT;
+	}
 
 	@ConfigItem(
 			position = 5,
-			keyName = "prayerColour",
-			name = "Prayer Colour",
-			description = "Choose the background colour of the prayer bar"
+			keyName = "healthLabelPosition",
+			name = "Health Label Position",
+			description = "Choose the location of the Health bar's label",
+			section = healthSettingsSection
 	)
-	default Color prayerColour() { return Color.CYAN; }
+	default LabelPlacement healthLabelPosition()
+	{
+		return LabelPlacement.TOP;
+	}
 
 
 	@ConfigItem(
 			position = 6,
-			keyName = "prayerSize",
-			name = "Prayer Size",
-			description = "Choose the size of the Prayer bar"
+			keyName = "showPrayer",
+			name = "Show Prayer",
+			description = "Render Prayer overlay"
 	)
-	default Dimension prayerSize() { return new Dimension(ComponentConstants.STANDARD_WIDTH, 0); }
+	default boolean renderPrayer() { return false; }
 
-
-	@ConfigItem
-			(
-					position = 7,
-					keyName = "showEnergy",
-					name = "Show Energy",
-					description = "Render Run Energy overlay"
-			)
-
-	default boolean renderEnergy() { return false; }
+	@ConfigSection(
+			position = 7,
+			name = "Prayer Bar Settings",
+			description = "Prayer Bar Settings"
+	)
+	String prayerSettingsSection = "prayerSettings";
 
 	@ConfigItem(
 			position = 8,
-			keyName = "energyColour",
-			name = "Energy Colour",
-			description = "Choose the background colour of the run energy bar"
+			keyName = "prayerSize",
+			name = "Prayer Bar Size",
+			description = "Choose the size of the Prayer bar",
+			section = prayerSettingsSection
 	)
-	default Color energyColour() { return Color.orange; }
+	@Range(
+			min = MiniBarsConstants.WIDTH_MIN
+	)
+	default Dimension prayerSize() { return new Dimension(ComponentConstants.STANDARD_WIDTH, MiniBarsConstants.STANDARD_HEIGHT); }
 
 	@ConfigItem(
 			position = 9,
-			keyName = "energySize",
-			name = "Energy Size",
-			description = "Choose the size of the Energy bar"
+			keyName = "prayerFullnessDirection",
+			name = "Prayer Fullness Direction",
+			description = "Choose the direction of fullness of the Prayer bar",
+			section = prayerSettingsSection
 	)
-	default Dimension energySize() { return new Dimension(ComponentConstants.STANDARD_WIDTH, 0); }
+	default FullnessDirection prayerFullnessDirection() { return FullnessDirection.RIGHT; }
 
-	@ConfigItem
-			(
-					position = 10,
-					keyName = "showSpec",
-					name = "Show Spec",
-					description = "Render Special Attack overlay"
-			)
+	@ConfigItem(
+			position = 10,
+			keyName = "prayerLabelPosition",
+			name = "Prayer Label Position",
+			description = "Choose the location of the Prayer bar's label",
+			section = prayerSettingsSection
+	)
+	default LabelPlacement prayerLabelPosition()
+	{
+		return LabelPlacement.TOP;
+	}
 
-	default boolean renderSpec() { return false; }
 
 	@ConfigItem(
 			position = 11,
-			keyName = "specColour",
-			name = "Spec Colour",
-			description = "Choose the background colour of the Spec bar"
+			keyName = "showEnergy",
+			name = "Show Energy",
+			description = "Render Run Energy overlay"
 	)
-	default Color specColour() { return Color.yellow; }
+	default boolean renderEnergy() { return false; }
+
+	@ConfigSection(
+			position = 12,
+			name = "Energy Bar Settings",
+			description = "Energy Bar Settings"
+	)
+	String energySettingsSection = "energySettings";
 
 	@ConfigItem(
-			position = 12,
-			keyName = "specSize",
-			name = "Spec Size",
-			description = "Choose the size of the Spec bar"
+			position = 13,
+			keyName = "energySize",
+			name = "Energy Bar Size",
+			description = "Choose the size of the Energy bar",
+			section = energySettingsSection
 	)
-	default Dimension specSize() { return new Dimension(ComponentConstants.STANDARD_WIDTH, 0); }
+	@Range(
+			min = MiniBarsConstants.WIDTH_MIN
+	)
+	default Dimension energySize() { return new Dimension(ComponentConstants.STANDARD_WIDTH, MiniBarsConstants.STANDARD_HEIGHT); }
 
-	@ConfigItem
-			(
-					position = 13,
-					keyName = "totalLabels",
-					name = "Total Labels",
-					description = "Show the label as a fraction of the whole value"
-			)
+	@ConfigItem(
+			position = 14,
+			keyName = "energyFullnessDirection",
+			name = "Energy Fullness Direction",
+			description = "Choose the direction of fullness of the Energy bar",
+			section = energySettingsSection
+	)
+	default FullnessDirection energyFullnessDirection() { return FullnessDirection.RIGHT; }
 
+	@ConfigItem(
+			position = 15,
+			keyName = "energyLabelPosition",
+			name = "Energy Label Position",
+			description = "Choose the location of the Energy bar's label",
+			section = energySettingsSection
+	)
+	default LabelPlacement energyLabelPosition()
+	{
+		return LabelPlacement.TOP;
+	}
+
+
+	@ConfigItem(
+			position = 16,
+			keyName = "showSpecial",
+			name = "Show Special",
+			description = "Render Special Attack overlay"
+	)
+	default boolean renderSpecial() { return false; }
+
+	@ConfigSection(
+			position = 17,
+			name = "Special Bar Settings",
+			description = "Special Bar Settings"
+	)
+	String specialSettingsSection = "specialSettings";
+
+	@ConfigItem(
+			position = 18,
+			keyName = "specialSize",
+			name = "Special Bar Size",
+			description = "Choose the size of the Special bar",
+			section = specialSettingsSection
+	)
+	@Range(
+			min = MiniBarsConstants.WIDTH_MIN
+	)
+	default Dimension specialSize() { return new Dimension(ComponentConstants.STANDARD_WIDTH, MiniBarsConstants.STANDARD_HEIGHT); }
+
+	@ConfigItem(
+			position = 19,
+			keyName = "specialFullnessDirection",
+			name = "Special Fullness Direction",
+			description = "Choose the direction of fullness of the Special bar",
+			section = specialSettingsSection
+	)
+	default FullnessDirection specialFullnessDirection() { return FullnessDirection.RIGHT; }
+
+	@ConfigItem(
+			position = 20,
+			keyName = "specialLabelPosition",
+			name = "Special Label Position",
+			description = "Choose the location of the Special bar's label",
+			section = specialSettingsSection
+	)
+	default LabelPlacement specialLabelPosition()
+	{
+		return LabelPlacement.TOP;
+	}
+
+
+	@ConfigItem(
+			position = 21,
+			keyName = "showLabels",
+			name = "Show Labels",
+			description = "Show labels with the minibars"
+	)
+	default boolean showLabels() { return true; }
+
+	@ConfigItem(
+			position = 22,
+			keyName = "totalLabels",
+			name = "Total Labels",
+			description = "Whether to display labels as the value or percentage of the total value"
+	)
 	default boolean totalLabels() { return true; }
 
-	@ConfigItem
-			(
-					position = 14,
-					keyName = "showLabels",
-					name = "Show Labels",
-					description = "Show labels on the minibars"
-			)
+	@ConfigItem(
+			position = 23,
+			keyName = "enableRestorationBars",
+			name = "Show Restores",
+			description = "Visually shows how much will be restored to your bars."
+	)
+	default boolean enableRestorationBars() { return true; }
 
-	default boolean showLabels() { return true; }
+	@ConfigItem(
+			position = 24,
+			keyName = "hideAfterCombatDelay",
+			name = "Hide after combat delay",
+			description = "Amount of ticks before hiding status bars after no longer in combat. 0 = always show status bars."
+	)
+	@Units(Units.TICKS)
+	default int hideAfterCombatDelay() { return 0; }
 
 }
--- src/main/java/com/skeldoor/MiniBarsEnergyOverlay.java
+++ src/main/java/com/skeldoor/MiniBarsEnergyOverlay.java
@@ -1,53 +1,124 @@
-package com.skeldoor;
+package net.runelite.client.plugins.minibars;
 
 import java.awt.*;
 import javax.inject.Inject;
 
-import net.runelite.api.Client;
+import net.runelite.api.*;
+import net.runelite.api.Point;
+import net.runelite.api.widgets.ComponentID;
+import net.runelite.api.widgets.Widget;
+import net.runelite.client.plugins.itemstats.Effect;
+import net.runelite.client.plugins.itemstats.ItemStatChangesService;
+import net.runelite.client.plugins.itemstats.StatChange;
 import net.runelite.client.ui.overlay.OverlayPanel;
+import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
-import net.runelite.client.ui.overlay.OverlayPriority;
 
 public class MiniBarsEnergyOverlay extends OverlayPanel{
 
-    @Inject
-    private Client client;
+    private static final Color ENERGY_HEAL_COLOR = new Color (199,  118, 0, 218);
+    private static final Color RUN_STAMINA_COLOR = new Color(160, 124, 72, 255);
+    private static final Color ENERGY_COLOR = new Color(199, 174, 0, 220);
+    private static final int MAX_RUN_ENERGY_VALUE = 100;
 
-    @Inject
-    private MiniBarsConfig config;
+    private final Client client;
+
+    private final MiniBarsPlugin plugin;
+
+    private final MiniBarsConfig config;
+
+    private final ItemStatChangesService itemStatService;
+
+    private MiniBarsComponent barRenderer;
 
     @Inject
     MiniBarsEnergyOverlay(
             Client client,
-            MiniBarsPlugin plugin)
+            MiniBarsPlugin plugin,
+            MiniBarsConfig config,
+            ItemStatChangesService itemstatservice)
     {
         super(plugin);
-        setPosition(OverlayPosition.ABOVE_CHATBOX_RIGHT);
-        setPriority(OverlayPriority.LOW);
+
+        //setPriority(OverlayPriority.LOW);
+        setPosition(OverlayPosition.DYNAMIC);
+        setLayer(OverlayLayer.ABOVE_WIDGETS);
         setMovable(true);
+        setResizable( false );
+        setSnappable( true );
         this.client = client;
+        this.plugin = plugin;
+        this.config = config;
+        this.itemStatService = itemstatservice;
+
+        initRenderer();
     }
 
+    private void initRenderer()
+    {
+        barRenderer = new MiniBarsComponent(
+                () -> MAX_RUN_ENERGY_VALUE,
+                () -> client.getEnergy() / 100,
+                () -> getRestoreValue("Run Energy"),
+                () ->
+                {
+                    if (client.getVarbitValue(Varbits.RUN_SLOWED_DEPLETION_ACTIVE) != 0)
+                    {
+                        return RUN_STAMINA_COLOR;
+                    }
+                    else
+                    {
+                        return ENERGY_COLOR;
+                    }
+                },
+                () -> ENERGY_HEAL_COLOR
+        );
+    }
 
     @Override
-    public Dimension render(Graphics2D graphics) {
-        if (config.renderEnergy()) renderEnergy();
+    public Dimension render( Graphics2D g )
+    {
+        if ( plugin.isBarsDisplayed() && config.renderEnergy() )
+        {
+            barRenderer.renderBar( config, g, panelComponent, config.energyFullnessDirection(), config.energyLabelPosition(), config.energySize().width, config.energySize().height );
 
-        return super.render(graphics);
+            return config.energySize();
+        }
+
+        return null;
     }
 
-    void renderEnergy(){
-        setPreferredSize(new Dimension(
-                config.energySize().width,
-                0));
-        MiniBarsUtils.buildPanel(
-                panelComponent,
-                0,
-                100,
-                (int)(client.getEnergy() / 100f),
-                config.energyColour(),
-                config.energySize(),
-                config.totalLabels(),
-                config.showLabels());
+    private int getRestoreValue(String skill)
+    {
+        final MenuEntry[] menu = client.getMenuEntries();
+        final int menuSize = menu.length;
+        if (menuSize == 0)
+        {
+            return 0;
+        }
+
+        final MenuEntry entry = menu[menuSize - 1];
+        final Widget widget = entry.getWidget();
+        int restoreValue = 0;
+
+        if (widget != null && widget.getId() == ComponentID.INVENTORY_CONTAINER)
+        {
+            final Effect change = itemStatService.getItemStatChanges(widget.getItemId());
+
+            if (change != null)
+            {
+                for (final StatChange c : change.calculate(client).getStatChanges())
+                {
+                    final int value = c.getTheoretical();
+
+                    if (value != 0 && c.getStat().getName().equals(skill))
+                    {
+                        restoreValue = value;
+                    }
+                }
+            }
+        }
+
+        return restoreValue;
     }
 }
